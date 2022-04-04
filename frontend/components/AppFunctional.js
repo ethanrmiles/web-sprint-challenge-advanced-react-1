e02829c099101dbefd2e6e645a1deaf90aa56ac0
Ethan Miles <ethanmilescs@gmail.com>
2022-04-04 13:15:58 -0500

    came up with a new way for navigating the grid. Works as expected, error messages are set into state

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -3,45 +3,74 @@ import { useState } from 'react'
 
 
 export default function AppFunctional(props) {  
-  const [findCurrentPositionIndex, setFindCurrentPositionIndex] = useState()
-  const [findCurrentPosition, setFindCurrentPosition] = useState()
-  const flatGrid = [null, null, null, null, "B", null, null, null, null]
+  const [position, setPosition] = useState(4)
+  const [message, setMessage] = useState()
 
-useEffect(() => {
-  setFindCurrentPositionIndex(flatGrid.findIndex(ind => ind === "B"))
-  setFindCurrentPosition(flatGrid.find(ind => ind === "B"))
-}, [])
+// const left = () =>  position > 0 ? setPosition(position -1) : position
+const left = () => {
+  if(position === 0 || position ===  3 || position ===  6 ){
+    setMessage("You can't go left")
+  } else {
+    setMessage(null)
+    setPosition(position -1)
+  }
+}
+
+const right = () => {
+  if(position === 2 || position ===  5 || position ===  8 ){
+    setMessage("You can't go right")
+  } else {
+    setMessage(null)
+    setPosition(position +1)
+  }
+}
+
+const up = () => {
+  if(position === 0 || position ===  1 || position ===  2 ){
+    setMessage("You can't go up")
+  } else {
+    setMessage(null)
+    setPosition(position -3)
+  }
+}
+// const down = () => setPosition(position + 3)
+const down = () => {
+  if(position === 6 || position ===  7 || position ===  8 ){
+    setMessage("You can't go down")
+  } else {
+    setMessage(null)
+    setPosition(position +3)
+  }
+}
+const reset = () => setPosition(4)
 
 
-
-//  const [x,y] = getCoordinates(grid)
-//  console.log(`${x}${y}`) // (1, 2)
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">{`Coordinates ${findCurrentPositionIndex}`}</h3>
+        <h3 id="coordinates">{`Coordinates ${position}`}</h3>
         <h3 id="steps">You moved 0 times</h3>
       </div>
       <div id="grid">
-        <div className={`square${flatGrid[0] === findCurrentPosition ? ' active' : ""}`}>{flatGrid[0] === findCurrentPosition ? "B" : 1}</div>
-        <div className={`square${flatGrid[1] === findCurrentPosition ? ' active' : ""}`}>{flatGrid[1] === findCurrentPosition ? "B" : 2}</div>
-        <div className={`square${flatGrid[2] === findCurrentPosition ? ' active' : ""}`}>{flatGrid[2] === findCurrentPosition ? "B" : 3}</div>
-        <div className={`square${flatGrid[3] === findCurrentPosition ? ' active' : ""}`}>{flatGrid[3] === findCurrentPosition ? "B" : 4}</div>
-        <div className={`square${flatGrid[4] === findCurrentPosition ? ' active' : ""}`}>{flatGrid[4] === findCurrentPosition ? "B" : 5}</div>
-        <div className={`square${flatGrid[5] === findCurrentPosition ? ' active' : ""}`}>{flatGrid[5] === findCurrentPosition ? "B" : 6}</div>
-        <div className={`square${flatGrid[6] === findCurrentPosition ? ' active' : ""}`}>{flatGrid[6] === findCurrentPosition ? "B" : 7}</div>
-        <div className={`square${flatGrid[7] === findCurrentPosition ? ' active' : ""}`}>{flatGrid[7] === findCurrentPosition ? "B" : 8}</div>
-        <div className={`square${flatGrid[8] === findCurrentPosition ? ' active' : ""}`}>{flatGrid[8] === findCurrentPosition ? "B" : 9}</div>
+        <div className={`square${position === 0 ? ' active' : ""}`}>{position === 0 ? "B" : 0}</div>
+        <div className={`square${position === 1 ? ' active' : ""}`}>{position === 1 ? "B" : 1}</div>
+        <div className={`square${position === 2 ? ' active' : ""}`}>{position === 2 ? "B" : 2}</div>
+        <div className={`square${position === 3 ? ' active' : ""}`}>{position === 3 ? "B" : 3}</div>
+        <div className={`square${position === 4 ? ' active' : ""}`}>{position === 4 ? "B" : 4}</div>
+        <div className={`square${position === 5 ? ' active' : ""}`}>{position === 5 ? "B" : 5}</div>
+        <div className={`square${position === 6 ? ' active' : ""}`}>{position === 6 ? "B" : 6}</div>
+        <div className={`square${position === 7 ? ' active' : ""}`}>{position === 7 ? "B" : 7}</div>
+        <div className={`square${position === 8 ? ' active' : ""}`}>{position === 8 ? "B" : 8}</div>
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{message}</h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
+        <button id="left"  onClick={left}>LEFT</button>
+        <button id="up" onClick={up}>UP</button>
+        <button id="right"  onClick={right}>RIGHT</button>
+        <button id="down" onClick={down}>DOWN</button>
+        <button id="reset" onClick={reset}>reset</button>
       </div>
       <form>
         <input id="email" type="email" placeholder="type email"></input>
